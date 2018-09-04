1e6078e05d0d1dfe93c56289ca231e4d88badf82
calmone <calmone91@gmail.com>
2018-09-04 11:41:27 +0900

    Add files via upload

---
+++ qrTest/QRCodeViewController.swift
@@ -0,0 +1,39 @@
+//
+//  QRCodeViewController.swift
+//  qrTest
+//
+//  Created by Taehyeon Han on 2018. 7. 24..
+//  Copyright © 2018년 Taehyeon Han. All rights reserved.
+//
+
+import UIKit
+
+class QRCodeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func unwindToHomeScreen(segue: UIStoryboardSegue) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
